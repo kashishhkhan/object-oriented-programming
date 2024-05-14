55e32f47c5512fb31e41ab853147b22e46a89268
kashishhkhan <kashishkhan6471@gmail.com>
2024-05-14 05:02:52 +0500

    push my oop to github

---
+++ index.ts
@@ -0,0 +1,58 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+class Student {
+    name:string
+    constructor(n:string){
+        this.name=n
+    }
+}
+
+class Person{
+    students:Student[]=[]
+    addStudent(obj:Student){
+        this.students.push(obj)
+    }
+}
+
+const persons = new Person()
+
+const programStart =async(persons:Person)=>{
+    do{
+    console.log("Welcome!");
+    const ans = await inquirer.prompt({
+        name: "select",
+        type:"list",
+        message: "Who would you like to engage with in a charming conversation?",
+        choices: ["staff", "student", "exit"]
+    })
+    if(ans.select == "staff"){
+        console.log("Welcome to the staff room! Feel free to ask any questions you have.")
+    }
+    else if(ans.select == "student"){
+        const ans = await inquirer.prompt({
+            name: "student",
+            type: "input",
+            message: "Please input the name of the student you'd like to interact with:"
+        })
+        const student = persons.students.find(val => val.name == ans.student)
+        if(!student){
+            const name = new Student(ans.student)
+            persons.addStudent(name)
+            console.log(`Hello! I am ${name.name}. Pleasure to connect with you!`);
+            console.log("New student added");
+            console.log("Current student list:");
+            console.log(persons.students);
+        } else {
+            console.log(`Hello I am ${student.name}. Wonderful to see you again!`);
+            console.log("Existing student list:");
+            console.log(persons.students);
+        }
+    }else if (ans.select == "exit"){
+        console.log("Exiting the program...");
+        process.exit()
+    }
+    }while(true)
+}
+
+programStart(persons)
